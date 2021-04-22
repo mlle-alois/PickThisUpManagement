b3c45e8531781f80a8038938f649817206123d4c
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2021-04-23 00:37:55 +0200

    Refacto requete et ajout get

--- Database/src/main/java/PostRequest.java
+++ Database/src/main/java/PostRequest.java
@@ -1,41 +1,17 @@
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 public class PostRequest {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 
-        var values = new HashMap<String, String>() {{
-            put("mail", "IchaiAdmin");
-            put ("password", "non");
-        }};
 
-        var objectMapper = new ObjectMapper();
-        String requestBody = objectMapper
-                .writeValueAsString(values);
+        User user = new User();
 
-       // HttpClient client = HttpClient.newHttpClient();
-        HttpClient client = Client.getInstance();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:3000/auth/login")).setHeader("Authorization","Bearer $2b$05$z4qQ9SkKwPZrLPTrfp7mAuKgxE9HvAp0Vw6rbpl32IJexh.8uedT.")
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                .build();
+        var testBody = new Body();
+        testBody.addValueToBody("mail","IchaiDev");
+        testBody.addValueToBody("password","non");
 
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-
-        Map<String, Object> map
-                = objectMapper.readValue(response.body(), new TypeReference<Map<String,Object>>(){});
-
-        System.out.println(response.body());
+        user.Login(testBody);
+        Board[] boards =  user.getBoard(testBody);
     }
 }
