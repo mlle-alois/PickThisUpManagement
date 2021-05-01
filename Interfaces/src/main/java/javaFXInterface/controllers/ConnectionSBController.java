ec810a57fc75e9ee6123ce3acd7bf13c362a1c89
PatateHunter <45663157+PatateHunter@users.noreply.github.com>
2021-05-01 22:24:32 +0200

    Déconnxion et switch de scene vers connexion OK

--- Interfaces/src/main/java/javaFXInterface/controllers/ConnectionSBController.java
+++ Interfaces/src/main/java/javaFXInterface/controllers/ConnectionSBController.java
@@ -53,4 +53,6 @@ public class ConnectionSBController {
   stage.show();
  }
 
+
+
 }
--- Interfaces/src/main/java/javaFXInterface/controllers/MenuController.java
+++ Interfaces/src/main/java/javaFXInterface/controllers/MenuController.java
@@ -7,14 +7,23 @@ import Models.Ticket;
 import Requete.Body;
 import Requete.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import lombok.SneakyThrows;
 
+import java.io.IOException;
+
 public class MenuController {
+    @FXML
+    private MenuBar menuBar;
     @FXML
     private MenuItem deconnexion;
     @FXML
@@ -23,6 +32,11 @@ public class MenuController {
     private Menu boardMenu;
     @FXML
     private Menu ticketMenu;
+    @FXML
+    private AnchorPane scenePane;
+    Stage root;
+    Stage stage;
+    Scene scene;
 
     String currentBoard;
 
@@ -33,6 +47,7 @@ public class MenuController {
 
         return user.getBoards(body);
     }
+
     @SneakyThrows
     public void initialize(User user) {
         this.user = user;
@@ -51,24 +66,24 @@ public class MenuController {
 
     private String[] parseBoards() throws JsonProcessingException {
         Board[] boards = getBoards(user);
-        String[] allBoars = new String[boards.length+1];
-        for (int i = 0; i < boards.length;i++){
-           allBoars[i] = boards[i].boardName;
+        String[] allBoars = new String[boards.length + 1];
+        for (int i = 0; i < boards.length; i++) {
+            allBoars[i] = boards[i].boardName;
         }
         allBoars[boards.length] = "Ajouter un tableau";
-    return  allBoars;
+        return allBoars;
     }
 
-    private MenuItem[] getBranchs(String[] boards){
+    private MenuItem[] getBranchs(String[] boards) {
         MenuItem[] menuItems = new MenuItem[boards.length];
 
-        for(int i = 0; i < boards.length;i++){
+        for (int i = 0; i < boards.length; i++) {
             menuItems[i] = new MenuItem(boards[i]);
         }
         return menuItems;
     }
 
-    public void selectItem(){
+    public void selectItem() {
       /*  TreeItem<String> item = (TreeItem<String>) treeBoard.getSelectionModel().getSelectedItem();
         if(item != null){
             System.out.println(item.getValue());
@@ -80,25 +95,26 @@ public class MenuController {
 
     private String[] parseTickets() throws JsonProcessingException {
         Status[] Tickets = getTickets(user);
-        String[] allTickets = new String[Tickets.length+1];
+        String[] allTickets = new String[Tickets.length + 1];
 
         allTickets[0] = "Tous les tickets";
 
-        for (int i = 0; i < Tickets.length;i++){
-            allTickets[i+1] = Tickets[i].statusLibelle;
+        for (int i = 0; i < Tickets.length; i++) {
+            allTickets[i + 1] = Tickets[i].statusLibelle;
         }
         return allTickets;
     }
 
     public Status[] getTickets(User user) throws JsonProcessingException {
         Body body = new Body();
-        body.addValueToBody("limit","3");
+        body.addValueToBody("limit", "3");
         return user.getStatus(body);
     }
-    private MenuItem[] getBranchsTickets(String[] Tickets){
+
+    private MenuItem[] getBranchsTickets(String[] Tickets) {
         MenuItem[] tasksItems = new MenuItem[Tickets.length];
 
-        for(int i = 0; i < Tickets.length;i++){
+        for (int i = 0; i < Tickets.length; i++) {
             tasksItems[i] = new MenuItem(Tickets[i]);
         }
 
@@ -106,8 +122,33 @@ public class MenuController {
     }
 
     // Déconnexion part
-    public boolean disconnect(){
+    public void disconnect(ActionEvent event) throws IOException {
+        if(!doesUserWantToDisconnect())
+            return;
+        Body body = new Body();
+       if(user.Logout(body))
+        switchToConnexionScene(event,"/Connection.fxml");
+    }
 
-        return true;
+    public void switchToConnexionScene(ActionEvent event,String ScenePath) throws IOException {
+
+       stage = (Stage) menuBar.getScene().getWindow();
+        stage.close();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(ScenePath));
+        root = loader.load();
+        root.setTitle("PickThis");
+        root.getIcons().add(new Image("/logo.PNG"));
+        root.show();
+
+    }
+
+    private boolean doesUserWantToDisconnect(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Déconnexion");
+        alert.setHeaderText("Vous êtes sur le point de vous déconnecter! ");
+        alert.setContentText("Est-ce vous sur de vouloir vous déconnecter? ");
+
+        return alert.showAndWait().get() == ButtonType.OK;
     }
 }
